96bc5a43fa92d77e205078ae3f5e0de55b7feb6a
za154295 <za154295@students.berufskolleg-borken.de>
2022-01-28 10:04:28 +0100

    icon/favicon

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+const meineApp = express()
+meineApp.set('view engine', 'ejs')
+meineApp.use(express.static('public'))
+meineApp.use(bodyParser.urlencoded({extended: true}))
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = meineApp.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-app.get('/',(req, res, next) => {              
-    res.render('index.ejs', {})          
+meineApp.get('/',(browserAnfrage, serverAntwort, next) => {              
+    serverAntwort.render('index.ejs', {})          
 })
 
 // Wenn die Login-Seite im Browser aufgerufen wird,...
 
-app.get('/login',(req, res, next) => {   
+meineApp.get('/login',(browserAnfrage, serverAntwort, next) => {   
     // ... dann wird die login.ejs vom Lerver gerendert und an den
     // Browser zurückgegeben:    
-    res.render('login.ejs', {})
+    serverAntwort.render('login.ejs', {})
 }) 
-    // die app.post('login') wird ausgeführt, sobald der Button auf dem Formular gedrückt wird.
-app.post('/login',(req, res, next) => {   
-        res.render('index.ejs', {})          
+    // die meineApp.post('login') wird ausgeführt, sobald der Button auf dem Formular gedrückt wird.
+meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {   
+        serverAntwort.render('index.ejs', {})          
 })
 
